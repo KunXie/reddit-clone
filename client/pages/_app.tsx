b33276b2e38351fb9f3a3c6b61709e50b6d1daa2
Kun Xie <alex.xiekun@gmail.com>
2021-04-10 14:25:55 -0500

    add navbar

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,12 +1,26 @@
-import "../styles/globals.css";
 import { AppProps } from "next/app";
 import Axios from "axios";
+
+import "../styles/globals.css";
+
+import Navbar from "../components/Navbar";
+import { Fragment } from "react";
+import { useRouter } from "next/router";
 
 Axios.defaults.baseURL = "http://localhost:5000/api";
 Axios.defaults.withCredentials = true; // trust server to set the cookie
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const { pathname } = useRouter();
+  const authRoutes = ["/register", "/login"];
+  const authRoute = authRoutes.includes(pathname);
+
+  return (
+    <Fragment>
+      {!authRoute && <Navbar />}
+      <Component {...pageProps} />
+    </Fragment>
+  );
 }
 
 export default MyApp;
